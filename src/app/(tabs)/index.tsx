23332a3349819e54a3023b16faea3cf1fd59f92a
smhunt <sean@ecoworks.ca>
2024-03-03 20:48:50 -0500

    2 Your first custom components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,38 +1,13 @@
-import { StyleSheet,Text,View,Image } from 'react-native';
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import Colors from '@/src/constants/Colors';
+import { View } from 'react-native';
 import products from '@/assets/data/products';  
+import ProductListItem from '@/src/components/ProductListItem';   
 
-const product = products[1];
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{uri: product.image}} style={styles.image} /> 
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+   <View>
+   <ProductListItem product={products[0]} />
+   <ProductListItem product={products[1]} />
+   </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
-  },
-  image:{
-    width: '100%',
-    aspectRatio:1,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: '600',
-    marginVertical: 10,
-  },
-  price: {  
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-  }
-});
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet,Text,View,Image } from 'react-native';
+
+import EditScreenInfo from '@/src/components/EditScreenInfo';
+import Colors from '@/src/constants/Colors';
+import products from '@/assets/data/products';  
+
+const ProductListItem = ({product}) => {
+ 
+  return (
+    <View style={styles.container}>
+      <Image source={{uri: product.image}} style={styles.image} /> 
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+}
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
+  },
+  image:{
+    width: '100%',
+    aspectRatio:1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+    marginVertical: 10,
+  },
+  price: {  
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+  }
+});
