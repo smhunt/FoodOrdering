c507a78a4d1d28b41b2b5d47bc3b71a846db8be5
smhunt <sean@ecoworks.ca>
2024-03-03 20:14:51 -0500

    getting remote synced with local

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -12,7 +12,6 @@ export default function TabOneScreen() {
       <Image source={{uri: product.image}} style={styles.image} /> 
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
-      
     </View>
   );
 }
