4034c2a300747f1b38ad3c8af1a46dd97bfee07a
smhunt <sean@ecoworks.ca>
2024-03-12 15:52:00 -0400

    Supabase local setup

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -15,8 +15,8 @@ const ExpoSecureStoreAdapter = {
   },
 };
 
-const supabaseUrl = 'https://ngkbttfagabwzsgzghhd.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5na2J0dGZhZ2Fid3pzZ3pnaGhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5MTcyOTEsImV4cCI6MjAyNTQ5MzI5MX0.Dz52ogzJcnqq8zkUX97D48uVRDj1-ykFAJAlnoUl6dA';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON || "";
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
