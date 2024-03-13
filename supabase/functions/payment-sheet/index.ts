a1afc7792bc30eb059fd7899a21563b1e1418129
smhunt <sean@ecoworks.ca>
2024-03-12 23:46:21 -0400

    FINALLY ALL FIXED (.env removed from gitgit status)

--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -44,6 +44,6 @@ serve(async (req) => {
   curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/payment-sheet' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
-    --data '{"amount":"Functions"}'
+    --data '{"amount":9999}'
 
 */
