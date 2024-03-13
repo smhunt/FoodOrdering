a8d3acd360836bdfd15ac4ce54181f52285f8390
smhunt <sean@ecoworks.ca>
2024-03-13 17:03:16 -0400

    Finished rememebering stripe payments

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,48 +2,45 @@ import { Alert } from "react-native";
 import { supabase } from "./supabase";
 import { initPaymentSheet, presentPaymentSheet } from "@stripe/stripe-react-native";
 
-
-const  fetchPaymentSheetParams = async (amount:number) => {
-    const {data,error} = await supabase.functions.invoke(
-        'payment-sheet', {
-            body:{amount},
-        }
-    );
-   if(data)   { 
-         return data;
-   } 
-   Alert.alert('Error fetching payment sheet params');
-   return {}; 
-};
-
-
-export const initialisePaymentSheet = async (amount:number) => {
-    console.log('Initialising payment sheet for: ',amount);
-
-    const {paymentIntent,publishableKey} =
-     await fetchPaymentSheetParams(
-        amount
-    );
-    
-     if(!paymentIntent || !publishableKey){
-         return;
-     }  
-
-    await initPaymentSheet({
-
-        merchantDisplayName: 'Supastore',        
-        paymentIntentClientSecret: paymentIntent,
-        defaultBillingDetails: {
-            name: 'Test User',
-    },
+const fetchPaymentSheetParams = async (amount: number) => {
+    const { data, error } = await supabase.functions.invoke('payment-sheet', {
+      body: { amount },
     });
-}
-
-export const openPaymentSheet = async () => {
-    const {error}  = await presentPaymentSheet();
-    if(error){
-        Alert.alert(error.message);
-        return false;
+    if (data) {
+      //console.log(data);
+      return data;
+    }
+    Alert.alert('Error fetching payment sheet params');
+    return {};
+  };
+  
+  export const initialisePaymentSheet = async (amount: number) => {
+    //console.log('Initialising payment sheet, for: ', amount);
+  
+    const { paymentIntent, publishableKey, customer, ephemeralKey } =
+      await fetchPaymentSheetParams(amount);
+  
+    if (!paymentIntent || !publishableKey) return;
+  
+    const result = await initPaymentSheet({
+      merchantDisplayName: 'Ecoworks',
+      paymentIntentClientSecret: paymentIntent,
+      customerId: customer,
+      customerEphemeralKeySecret: ephemeralKey,
+      defaultBillingDetails: {
+        name: 'Jane Doe',
+      },
+      returnURL: 'payments-example://stripe-redirect',
+    });
+    //console.log(result);
+  };
+  
+  export const openPaymentSheet = async () => {
+    const { error } = await presentPaymentSheet();
+  
+    if (error) {
+      Alert.alert(error.message);
+      return false;
     }
     return true;
-}
+  };
