c696a7bdf38aa6578148aa03bed7aa7130e38e15
smhunt <sean@ecoworks.ca>
2024-03-12 21:30:48 -0400

    Create stripe payment intent with SUPABASE EDGE FUNCTION

---
+++ supabase/functions/payment-sheet/index.ts
@@ -0,0 +1,49 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
+import { stripe } from '../_utils/stripe.ts';
+
+serve(async (req) => {
+
+  try {
+    const { amount } = await req.json()
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: 'cad',
+    })
+
+    const res = {
+      paymentIntent: paymentIntent.client_secret,
+      publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
+    }
+
+    return new Response(
+      JSON.stringify(res),
+      { headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.log(error)
+    return new Response(
+      JSON.stringify({ error: error }),
+      { headers: { "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
+});
+
+
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/payment-sheet' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"amount":"Functions"}'
+
+*/
